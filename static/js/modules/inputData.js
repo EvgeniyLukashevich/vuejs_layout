3fe20986282b394171106e63cc29fcf4b1c2afab
Evgeniy Phive <phivedec@gmail.com>
2024-06-19 22:36:09 +0800

    the 2nd homework fixed (sorted posts by date added)

---
+++ static/js/modules/inputData.js
@@ -0,0 +1,103 @@
+const blogItems = [
+    {
+        href: "#",
+        image: "./public/images/blog1.jpeg",
+        imageMin: "./public/images/blog1-min.png",
+        tag: "Дизайн Кухни",
+        title: "Создадим лучший макет перепланировки",
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, architecto aspernatur at aut " +
+            "exercitationem laudantium obcaecati odit repellat repudiandae sed similique sint tempora tempore, " +
+            "temporibus velit, veniam voluptatibus? Cum dolorem ea facere iste laudantium molestias mollitia nam, " +
+            "nihil obcaecati odio, quae quasi repellat soluta tenetur ullam vel velit veniam voluptate voluptatem " +
+            "voluptatibus! Assumenda delectus dolor doloribus earum esse, harum ipsa itaque neque, optio ratione " +
+            "recusandae soluta? Alias beatae cum distinctio et numquam, suscipit.",
+        quote: "Quae quasi repellat soluta",
+        date: "19 декабря, 2022"
+    },
+    {
+        href: "#",
+        image: "./public/images/blog2.jpeg",
+        imageMin: "./public/images/blog2-min.png",
+        tag: "Дизайн для жизни",
+        title: "Лучшие интерьерные идеи по низкой цене",
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus architecto, at autem beatae " +
+            "esse eveniet exercitationem illo illum inventore ipsam maiores necessitatibus, odit, quas sit " +
+            "tempore temporibus unde? Accusamus architecto blanditiis dolorem doloribus illo iure " +
+            "natus necessitatibus.",
+        quote: "Architecto blanditiis dolorem doloribus",
+        date: "22 декабря, 2022"
+    },
+    {
+        href: "#",
+        image: "./public/images/blog3.jpeg",
+        imageMin: "./public/images/blog3-min.png",
+        tag: "Дизайн Интерьера",
+        title: "Лучшие интерьерные решения для офисов",
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa, deserunt eaque eius eveniet, " +
+            "exercitationem fuga illo illum iure laborum laudantium maxime nam nisi sapiente sit vel. Dignissimos " +
+            "fugit ipsam itaque maiores repellat saepe tempora! Cum ea explicabo, fugit magni non nostrum " +
+            "reprehenderit sit ut veritatis?",
+        quote: "Dignissimos fugit ipsam itaque maiores repellat saepe tempora!",
+        date: "25 декабря, 2022"
+    },
+    {
+        href: "#",
+        image: "./public/images/blog4.jpeg",
+        imageMin: "./public/images/blog4-min.png",
+        tag: "Дизайн Кухни",
+        title: "Создадим лучший макет перепланировки",
+        text: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. A consequuntur excepturi nobis qui sunt " +
+            "veritatis? Aperiam cupiditate laboriosam laudantium magnam, minima praesentium sint vel voluptatibus.",
+        quote: "Minima praesentium sint vel voluptatibus",
+        date: "21 декабря, 2022"
+    },
+    {
+        href: "#",
+        image: "./public/images/blog5.jpeg",
+        imageMin: "./public/images/blog5-min.png",
+        tag: "Дизайн для жизни",
+        title: "Лучшие интерьерные идеи по низкой цене",
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias enim est et ex non porro " +
+            "praesentium vel! Accusamus accusantium, alias aliquid aperiam asperiores aut delectus dolores est " +
+            "eveniet expedita fuga illum inventore ipsum labore magnam modi molestiae, nam nemo neque non nulla " +
+            "odio optio pariatur placeat praesentium provident quaerat quibusdam quis quisquam ratione recusandae " +
+            "rem rerum sed sint sit soluta tempora. Dolores dolorum earum, fuga incidunt ipsam laboriosam, nam " +
+            "nihil numquam optio quasi quod rerum, sequi veniam! Adipisci alias animi asperiores at consequuntur " +
+            "dolorum facere fuga, illo ipsum laudantium magnam natus odit placeat quae quia quisquam quo recusandae," +
+            " repellat suscipit veritatis. Alias exercitationem expedita facere quae rem repellat? Beatae eius " +
+            "eligendi ipsa ipsam laborum maiores natus, numquam officiis optio, quam reiciendis reprehenderit " +
+            "repudiandae sequi? Culpa dignissimos inventore perferendis quidem sunt.",
+        quote: "Adipisci alias animi asperiores",
+        date: "22 декабря, 2022"
+    },
+    {
+        href: "#",
+        image: "./public/images/blog6.jpeg",
+        imageMin: "./public/images/blog6-min.png",
+        tag: "Дизайн Интерьера",
+        title: "Лучшие интерьерные решения для офисов",
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ad alias assumenda beatae, commodi " +
+            "consectetur consequatur enim eveniet ex excepturi hic incidunt itaque libero minima modi nihil " +
+            "obcaecati optio provident quibusdam recusandae similique tempora velit voluptatibus! Aspernatur " +
+            "deserunt eligendi facilis fuga, in inventore iusto laudantium minima, necessitatibus " +
+            "omnis ratione saepe.",
+        quote: "enim eveniet ex excepturi hic incidunt itaque libero",
+        date: "23 декабря, 2022"
+    },
+    {
+        href: "#",
+        image: "./public/images/blog7.png",
+        imageMin: "./public/images/blog7-min.png",
+        tag: "Дизайн Интерьера",
+        title: "Low Cost Latest Invented Interior Designing Ideas",
+        text: "С того времени этот, похожий на латинский, текст стал стандартом в печатной промышленности для " +
+            "примеров шрифтов и текстов. Перед появлением электронных издательств дизайнеры импровизировали в " +
+            "работе над макетами, изображая текст при помощи волнистых линий. С появлением самоклеющихся наклеек " +
+            "с напечатанным текстом «Lorem ipsum» появился более реалистичный способ обозначения расположения " +
+            "текста на странице.",
+        quote: "Дизайнеры импровизировали в работе над макетами",
+        date: "30 декабря, 2022"
+    },
+]
+
+export {blogItems}
--- static/js/script.js
+++ static/js/script.js
@@ -1,61 +1,5 @@
-const blogItems = [
-    {
-        href: "#",
-        image: "./public/images/blog1.png",
-        tag: "Дизайн Кухни",
-        title: "Создадим лучший макет перепланировки",
-        text: "",
-        date: "26 декабря, 2022"
-    },
-    {
-        href: "#",
-        image: "./public/images/blog2.png",
-        tag: "Дизайн для жизни",
-        title: "Лучшие интерьерные идеи по низкой цене",
-        text: "",
-        date: "22 декабря, 2022"
-    },
-    {
-        href: "#",
-        image: "./public/images/blog3.png",
-        tag: "Дизайн Интерьера",
-        title: "Лучшие интерьерные решения для офисов",
-        text: "",
-        date: "25 декабря, 2022"
-    },
-    {
-        href: "#",
-        image: "./public/images/blog4.png",
-        tag: "Дизайн Кухни",
-        title: "Создадим лучший макет перепланировки",
-        text: "",
-        date: "26 декабря, 2022"
-    },
-    {
-        href: "#",
-        image: "./public/images/blog5.png",
-        tag: "Дизайн для жизни",
-        title: "Лучшие интерьерные идеи по низкой цене",
-        text: "",
-        date: "22 декабря, 2022"
-    },
-    {
-        href: "#",
-        image: "./public/images/blog6.png",
-        tag: "Дизайн Интерьера",
-        title: "Лучшие интерьерные решения для офисов",
-        text: "",
-        date: "25 декабря, 2022"
-    },
-    {
-        href: "#",
-        image: "./public/images/blog_new.png",
-        tag: "Дизайн Интерьера",
-        title: "Low Cost Latest Invented Interior Designing Ideas",
-        text: "С того времени этот, похожий на латинский, текст стал стандартом в печатной промышленности для примеров шрифтов и текстов. Перед появлением электронных издательств дизайнеры импровизировали в работе над макетами, изображая текст при помощи волнистых линий. С появлением самоклеющихся наклеек с напечатанным текстом «Lorem ipsum» появился более реалистичный способ обозначения расположения текста на странице.",
-        date: "26 декабря, 2022"
-    },
-]
+import {blogItems} from "./modules/inputData.js";
+import {parseDate} from "./modules/functions.js";
 
 const blogApp = {
     data() {
@@ -66,10 +10,15 @@ const blogApp = {
         }
     },
     computed: {
+        sortedByDateItems() {
+            return this.items.sort((a, b) => {
+                return parseDate(b.date) - parseDate(a.date);
+            })
+        },
         lastItem() {
-            return this.items[this.items.length - 1]
-        }
-    }
+            return this.sortedByDateItems[0]
+        },
+    },
 }
 
 Vue.createApp(blogApp).mount("#blog_page_app")
